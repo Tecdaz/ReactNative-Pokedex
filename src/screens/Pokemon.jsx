171a1e2b9f85ac24c8487a995748bd3d97d3dad5
Santiago Arias <santi99oca@gmail.com>
2023-11-29 18:35:39 -0300

    Add header icons to Pokemon screen

--- src/screens/Pokemon.jsx
+++ src/screens/Pokemon.jsx
@@ -1,6 +1,7 @@
 import { ScrollView } from "react-native";
 import React, { useEffect, useState } from "react";
 import { getPokemonDetailsApi } from "../api/pokemon";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
 import Stats from "../components/Pokemon/Stats";
@@ -11,6 +12,23 @@ export default function Pokemon(props) {
   } = props;
 
   const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Icon name="heart" color={"#fff"} size={20} marginRight={16} />
+      ),
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          color={"#fff"}
+          size={20}
+          marginLeft={16}
+          onPress={() => navigation.goBack()}
+        />
+      ),
+    });
+  }, [navigation, params]);
 
   useEffect(() => {
     (async () => {
