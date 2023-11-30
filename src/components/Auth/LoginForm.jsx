b6ad9e8756c889f3f17c722248106892900f5d91
Santiago Arias <santi99oca@gmail.com>
2023-11-30 16:16:52 -0300

    Refactor login form and account screen

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -1,10 +1,81 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  Keyboard,
+} from "react-native";
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function LoginForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <View>
-      <Text>Login</Text>
+      <Text style={styles.title}>Iniciar Sesión</Text>
+      <TextInput
+        placeholder="Nombre de usuario"
+        style={styles.input}
+        autoCapitalize="none"
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue("username", text)}
+      />
+      <TextInput
+        placeholder="Contraseña"
+        style={styles.input}
+        autoCapitalize="none"
+        secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
+      />
+      <Button title="Iniciar Sesión" onPress={formik.handleSubmit} />
+
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
     </View>
   );
 }
+
+function initialValues() {
+  return {
+    username: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    username: Yup.string().required("El ususario es obligatorio"),
+    password: Yup.string().required("La contraseña es obligatoria"),
+  };
+}
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 16,
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    margin: 16,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+  error: {
+    textAlign: "center",
+    color: "red",
+    marginTop: 16,
+  },
+});
